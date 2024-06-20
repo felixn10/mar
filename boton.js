2fafd10b768eb04017c210bbdffd134cf56bc910
felixn10 <noea8785@gmail.com>
2024-06-19 21:49:45 -0700

    añadir archivo index.html

---
+++ boton.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+document.getElementById('toggle-menu').addEventListener('click', function() {
+    var menu = document.getElementById('menu-servicios');
+    menu.classList.toggle('show');
+});
